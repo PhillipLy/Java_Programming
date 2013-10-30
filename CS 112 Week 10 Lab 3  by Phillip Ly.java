ab019981d9be00b6bc06fcfb5397ae3511fa032b
Phillip <tetsuprucism@gmail.com>
2013-10-29 21:21:59 -0700

    I commit Week 10 Lab 3

---
+++ CS 112 Week 10 Lab 3  by Phillip Ly.java
@@ -0,0 +1,73 @@
+// CS 112 Week 10 Lab 3
+// by: Phillip Ly
+// This is a class of the program guess a number game
+// with "dice" implemented 
+
+import javax.swing.JOptionPane; // imports that makes Java option Panels implementation possible
+
+//starts of class Dice
+public class Dice {
+	
+	//declarations
+	int diceNumber;
+	int number;
+	int bouces;
+	
+	// Constructor
+	// bounceNumber is the number of bounces from the main program
+	
+	public Dice (int bounceNumber)
+	{
+		bounces = bounceNumber;
+	}
+	
+	// performing the dice function of generating random number from 1 to 6
+	public int Throw(int bounces) {
+		
+	JOptionPane.showMessageDialog(null,"The current bounces are " + bounces);	
+	
+	// if the bounces is 1
+	if(bounces == 1){
+	
+		int random1 = (1 + (int) (Math.random() * 6));
+
+	}
+	// if the bounces are 2
+	else if (bounces == 2){
+		int random1 = (1 + (int) (Math.random() * 6));
+		int random2 = (1 + (int) (Math.random() * 6));
+		 number = (random1+random2 / 2);
+
+	}
+	 
+	 return number;
+	}
+	
+	public int Value()
+	{
+		diceNumber = number;
+		return diceNumber;
+	}
+
+} // end class Dice
+
+// starts of main program for testing
+import javax.swing.JOptionPane;
+
+
+public class test {
+	public static void main(String[] args) {
+		
+		
+		int bounces = (int)(Math.random() *(2) + 1;
+		Dice test = new dice(bounces);
+		int bounces;		
+		 
+		test.Throw(bounces);
+		
+		JOptionPane.showMessageDialog(null,"The test number is " + test.Value());
+	
+	
+	}
+
+}
