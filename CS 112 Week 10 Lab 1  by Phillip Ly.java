b01eff4866ed7d8a85d76c5f2da12c45590c5420
Phillip <tetsuprucism@gmail.com>
2013-10-29 20:30:53 -0700

    I commit lab 1 Week 10 file

---
+++ CS 112 Week 10 Lab 1  by Phillip Ly.java
@@ -0,0 +1,23 @@
+// CS 112 Week 10 Lab 1
+// by: Phillip Ly
+// This is a class of the program guess a number game
+// with "dice" implemented 
+
+import javax.swing.JOptionPane; // imports that makes Java option Panels implementation possible
+//starts of class Dice
+
+public class Dice {
+
+	// performing the dice function of generating random number from 1 to 6
+	public int Throw() {
+
+		return (1 + (int) (Math.random() * 6));
+	}
+
+	public int Value() {
+
+		int currentDiceValue = (1 + (int) (Math.random() * 6));
+		return currentDiceValue;
+	}
+
+} // end class Dice
