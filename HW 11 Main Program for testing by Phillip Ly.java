e56f3154e460a671bc95db4074b2ae471fb7642e
tetsuprucism <tetsuprucism@gmail.com>
2013-11-11 21:20:21 -0800

    Update HW 11 Main Program for testing by Phillip Ly.java

--- HW 11 Main Program for testing by Phillip Ly.java
+++ HW 11 Main Program for testing by Phillip Ly.java
@@ -23,9 +23,12 @@ public class mainProgramTest_Week_11_HW {
 		System.out.println(original.equals(cloned));
 		
 		// test equal method using numberOfThrows and valuePerThrow for both original and clones
+		
+		System.out.println("CPU is checking the original class to see if number of throws equal to the value of throw (returning true if yes, false if no)");
 		original.areEqual();
 		original.areNotEqual();
 		
+		System.out.println("CPU is checking the cloned class to see if number of throws equal to the value of throw (returning true if yes, false if no)");
 		cloned.areEqual();
 		cloned.areNotEqual();
 		
